5bca9ab65b8d85fbc541e0dc17a462110fcce3ba
cburbano <cburbano@ubits.co>
2024-01-29 22:50:52 -0500

    add fetch adapter

--- TypeScript/src/Patterns/FactoryMethod/Ejemplos/Http/interface/http-adapter.interface.ts
+++ TypeScript/src/Patterns/FactoryMethod/Ejemplos/Http/interface/http-adapter.interface.ts
@@ -1,6 +1,7 @@
 export interface IHttpAdapter<T> {
-  get({ url, args }: { url: string; args?: any }): Promise<T>
-  post({ url, body, args }: { url: string; body: T; args?: any }): Promise<void>
-  put({ url, body, args }: { url: string; body: T; args?: any }): Promise<void>
-  delete({ url, body, args }: { url: string; body: T; args?: any }): Promise<void>
+  get({ url, args }: { url: string; args?: any }): Promise<Response>
+  post({ url, body, args }: { url: string; body: T; args?: any }): Promise<Response>
+  put({ url, body, args }: { url: string; body: T; args?: any }): Promise<Response>
+  patch({ url, body, args }: { url: string; body: T; args?: any }): Promise<Response>
+  delete({ url, body, args }: { url: string; body: T; args?: any }): Promise<Response>
 }
--- TypeScript/src/Patterns/FactoryMethod/Ejemplos/Http/type/fetch-adapter.ts
+++ TypeScript/src/Patterns/FactoryMethod/Ejemplos/Http/type/fetch-adapter.ts
@@ -1,27 +1,60 @@
 import { IHttpAdapter } from '../interface/http-adapter.interface'
 
 export class FetchAdapter<T> implements IHttpAdapter<T> {
-  private _headers: Headers
-  private _baseUrl: string
+  async get({ url, args }: { url: string; args?: any }): Promise<Response> {
+    const headers = new Headers({
+      ...args.headers,
+    })
 
-  /**
-   *
-   */
-  constructor(baseUrl: string, headers: Headers) {
-    this._baseUrl = baseUrl
-    this._headers = headers
+    const init: RequestInit = { method: 'GET', headers }
+    const request = new Request(url, init)
+    let result = await this.httpFetch(request).catch(err => {
+      throw err
+    })
+
+    return await result.json()
   }
-  get({ url, args }: { url: string; args?: any }): Promise<T> {
-    throw new Error('Method not implemented.')
+  async post({ url, body, args }: { url: string; body: T; args?: any }): Promise<any> {
+    const headers = new Headers({
+      ...args.headers,
+    })
+    const init: RequestInit = { method: 'POST', headers: headers, body: JSON.stringify(body) }
+    const request = new Request(url, init)
+    const response = await this.httpFetch(request)
+    return response
   }
-  post({ url, body, args }: { url: string; body: T; args?: any }): Promise<void> {
-    throw new Error('Method not implemented.')
+  async put({ url, body, args }: { url: string; body: T; args?: any }): Promise<any> {
+    const init: RequestInit = {
+      method: 'PUT',
+      headers: args.headers,
+      body: JSON.stringify(body),
+    }
+    const request = new Request(url, init)
+    const response = await this.httpFetch(request)
+    return response
   }
-  put({ url, body, args }: { url: string; body: T; args?: any }): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async patch({ url, body, args }: { url: string; body: T; args?: any }): Promise<Response> {
+    const init: RequestInit = {
+      method: 'PATCH',
+      headers: args.headers,
+      body: JSON.stringify(body),
+    }
+    const request = new Request(url, init)
+    const response = await this.httpFetch(request)
+    return response
   }
-  delete({ url, body, args }: { url: string; body: T; args?: any }): Promise<void> {
-    throw new Error('Method not implemented.')
+  async delete({ url, body, args }: { url: string; body: T; args?: any }): Promise<any> {
+    const init: RequestInit = {
+      method: 'DELETE',
+      headers: args.headers,
+    }
+
+    if (body) init['body'] = JSON.stringify(body)
+
+    const request = new Request(url, init)
+    const response = await this.httpFetch(request)
+    return response
   }
 
   private async httpFetch(request: Request): Promise<Response> {
